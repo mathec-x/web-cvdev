cac5dcb619947490cbe263ba345da115fa4fabfa
matheus correa <mathec@live.com>
2022-02-28 14:15:36 -0300

    fix: clean code for perfil

--- src/pages/Candidate/index.js
+++ src/pages/Candidate/index.js
@@ -2,29 +2,20 @@ import React from 'react';
 
 import List from '@mui/material/List';
 import Grid from '@mui/material/Grid';
-import Avatar from '@mui/material/Avatar';
-import { Div, Container, StyledListItem } from '../../components';
+import { Div, Container } from '../../components';
 import { useSocket } from 'socket.io-hook';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ListSubheader, Typography, IconButton, ListItemSecondaryAction } from '@mui/material';
 import { AddCircleIcon } from '../../components/Icons';
-import Candidate from '../../services/Candidate';
+import Perfil from './perfil';
+
 
 const PageCandidate = () => {
 
     const candidate = useSelector(state => state.candidate);
     const candidates = useSelector(state => state.candidates);
 
-    const update = React.useCallback((title, label, prop) => {
-        return window.Prompt(title, [{
-            type:'text', label: label, name: prop
-        }])
-        .then(data => {
-            Candidate.update(candidate.uuid, data);
-        })
-
-    }, [candidate])
 
     const isMyCandidate = React.useMemo(() => {
         return candidates.findIndex(e => e.uuid === candidate.uuid) !== -1;
@@ -45,23 +36,7 @@ const PageCandidate = () => {
     return (
         <Container spacing={2} p={2}>
             <Grid item xs={12} md={3} sx={{ minHeight: '88vh', borderRight: 'thin solid #eee' }}>
-                <Div show={isMyCandidate}>
-                    <Typography>Meu Currículo</Typography>
-                </Div>
-                <Div>
-                    <IconButton onClick={() => update('Atualizar imagem de perfil', 'Cole uma url valida', 'image')}>
-                        <Avatar
-                            alt={candidate.name}
-                            src={candidate.image}
-                            sx={{ width: 106, height: 106 }}
-                        />
-                    </IconButton>
-                </Div>
-                <List dense subheader={<ListSubheader><Typography>Perfil</Typography></ListSubheader>}>
-                    <StyledListItem button={isMyCandidate} primary='Nick' secondary={candidate.nick} />
-                    <StyledListItem button={isMyCandidate} primary='Nome' secondary={candidate.name} />
-                    <StyledListItem button={isMyCandidate} primary='Email' secondary={candidate.email} />
-                </List>
+                <Perfil candidate={candidate} permission={isMyCandidate} />
             </Grid>
             <Grid item xs={12} md={3} sx={{ borderRight: 'thin solid #eee' }}>
                 <List dense subheader={(
---
+++ src/pages/Candidate/perfil.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Candidate from '../../services/Candidate';
+
+import { useNavigate } from 'react-router-dom';
+import { StyledListItem, Div } from '../../components';
+
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListSubheader from '@mui/material/ListSubheader';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+
+const Perfil = ({ candidate, permission }) => {
+    const navigate = useNavigate()
+
+    const update = React.useCallback((
+        /** @type {string} */ title,
+        /** @type {any}    */ input
+    ) => {
+        return window
+            .Prompt(title, [{ type: 'text', ...input }])
+            .then(data => Candidate
+                .update(candidate.uuid, data)
+                .then((res) => {
+                    if (input.name === 'nick') {
+                        navigate(`/candidate/${data.nick}`);
+                    }
+                })
+            )
+
+    }, [candidate])
+
+    return (<>
+        <Div show={permission}>
+            <Typography>Meu Currículo</Typography>
+        </Div>
+        <Div>
+            <IconButton onClick={() => update('Atualizar imagem de perfil', {
+                label: 'Cole uma url valida',
+                name: 'image'
+            })}>
+                <Avatar
+                    alt={candidate.name}
+                    src={candidate.image}
+                    sx={{ width: 106, height: 106 }}
+                />
+            </IconButton>
+        </Div>
+        <List dense subheader={<ListSubheader><Typography>Perfil</Typography></ListSubheader>}>
+            <StyledListItem
+                onClick={() => update('Atualizar Nickname', {
+                    label: 'Informe o novo apelido, inicie com @',
+                    name: 'nick'
+                })}
+                button={permission}
+                primary='Nick'
+                secondary={candidate.nick}
+            />
+            <StyledListItem
+                onClick={() => update('Atualizar Nome', {
+                    label: 'Informe o novo Nome',
+                    name: 'name'
+                })}
+                button={permission}
+                primary='Nome'
+                secondary={candidate.name}
+            />
+            <StyledListItem
+                onClick={() => update('Atualizar Email', {
+                    label: 'Informe o novo email',
+                    name: 'email'
+                })}
+                button={permission}
+                primary='Email'
+                secondary={candidate.email}
+            />
+        </List>
+    </>)
+}
+
+export default Perfil;
