bb18e901e7ce6d1858e6c2828eead8bedc49e92f
matheus correa <mathec@live.com>
2022-03-14 12:10:00 -0300

    fix: upgrade pwa lib

--- src/contexts/Pwa.js
+++ src/contexts/Pwa.js
@@ -4,25 +4,29 @@ import ReactPwa from 'react-pwa-app';
 const Pwa = (props) => {
   return (
     <ReactPwa
-      test
       config={{
         swUrl: '/service-worker.js',
-        onError: () => {
-          // alert('erro ao instalar sw');
+        onError: (error) => {
+          console.log('[service worker error]', error);
         },
-        onSuccess: ( e) => {
-          e.sync.register('')
-          // console.log('service worker instalado');
+        onSuccess: (e) => {
+          // e.sync.register('')
+          console.log('[service worker instalado]', e);
         },
-        onUpdate: () => {
+        onUpdate: (e) => {
           // eslint-disable-next-line no-alert
+          console.log('[service worker atualizado]', e);
           alert('Feche o aplicativo e abra novamente para instalar atualizações');
           window.close();
         },
         onPrompt: (e) => {
+          console.log('service worker prompt', e);
           if(e.outcome === 'accepted'){
             window.location.reload()
           }
+        },
+        onOffline: () => {
+          console.log('service worker offline');
         }
       }}
       {...props}
